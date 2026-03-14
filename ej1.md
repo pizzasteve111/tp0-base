2e7f86fa726c7a84b27f83698b4e8e04bdf29b54
pizzasteve111 <juancardoso123456@gmail.com>
2026-03-14 10:44:09 -0300

    generador de yaml listo

---
+++ ej1.md
@@ -0,0 +1,5 @@
+Lo que tengo que hacer es que mi bash al ejecutarse me genere un compose nuevo con soporte para python, go e incluya los containers que instancio.
+
+Al correr el bash, se me genera el nuevo yaml con esa config, luego le hago compose up a ese yaml nuevo.
+
+DAR PERMISOS AL BASH => chmod +x generar-compose.sh
--- generar-compose.sh
+++ generar-compose.sh
@@ -9,12 +9,51 @@ if [ -z "$COMPOSE_FILE" ] || [ -z "$TOTAL_CLIENTS" ]; then
     exit 1
 fi
 
-if [ ! -f "$COMPOSE_FILE" ]; then
-    
-    exit 2
-fi
 
 echo "hay $TOTAL_CLIENTS clientes levantados "
 
-# El nombre del servicio en docker-compose es 'client' según el repo
-docker compose -f "$COMPOSE_FILE" up --build --scale client="$TOTAL_CLIENTS"
+#las <> indican la dirección, con cat. Osea todo va a parar al output file
+cat > "$COMPOSE_FILE" <<EOF
+name: tp0
+services:
+  server:
+    container_name: server
+    image: server:latest
+    entrypoint: python3 /main.py
+    environment:
+      - PYTHONUNBUFFERED=1
+      - LOGGING_LEVEL=DEBUG
+    networks:
+      - testing_net
+EOF
+
+#levanto un container por cada uno de los N clientes,
+for ((i=1;i<=TOTAL_CLIENTS;i++))
+do
+cat >> "$COMPOSE_FILE" <<EOF
+
+  client$i:
+    container_name: client$i
+    image: client:latest
+    entrypoint: /client
+    environment:
+      - CLI_ID=$i
+      - CLI_LOG_LEVEL=DEBUG
+    depends_on:
+      - server
+    networks:
+      - testing_net
+EOF
+done
+
+cat >> "$COMPOSE_FILE" <<EOF
+
+networks:
+  testing_net:
+    ipam:
+      driver: default
+      config:
+        - subnet: 172.25.125.0/24
+EOF
+
+##entonces con esto hago que se me genere el compose.yaml con todas estas configuraciones que le pido
