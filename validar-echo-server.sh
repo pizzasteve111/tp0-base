853d23366fa5ada5acd1e825e67876abda59c9ba
pizzasteve111 <juancardoso123456@gmail.com>
2026-03-15 15:20:11 -0300

    cambios al ej3

--- validar-echo-server.sh
+++ validar-echo-server.sh
@@ -2,20 +2,11 @@
 
 
 
-NETWORK=test-net
+NETWORK=tp0-base_testing_net
 SERVER_CONTAINER=server
-SERVER_IMAGE=server:latest
 PORT=12345
 MESSAGE="ping"
-docker rm -f $SERVER_CONTAINER 2>/dev/null || true
-docker network create $NETWORK >/dev/null 2>&1
 
-docker run -d \
-    --name $SERVER_CONTAINER \
-    --network $NETWORK \
-    $SERVER_IMAGE >/dev/null
-
-sleep 5
 
 
 MSG_RESPONSE=$(docker run --rm --network $NETWORK \
