9472c99571973c38af31c9248c8f830fa5f6ee8e
pizzasteve111 <juancardoso123456@gmail.com>
2026-03-16 17:34:13 -0300

    hardcodeo env var

--- generar-compose.sh
+++ generar-compose.sh
@@ -38,11 +38,11 @@ cat >> "$COMPOSE_FILE" <<EOF
     entrypoint: /client
     environment:
       - CLI_ID=$i
-      - NOMBRE=\${NOMBRE}
-      - APELLIDO=\${APELLIDO}
-      - DOCUMENTO=\${DOCUMENTO}
-      - NACIMIENTO=\${NACIMIENTO}
-      - NUMERO=\${NUMERO}
+      - NOMBRE=Santiago
+      - APELLIDO=Lorca
+      - DOCUMENTO=30904465
+      - NACIMIENTO=1999-03-17
+      - NUMERO=7574
     depends_on:
       - server
     networks:
